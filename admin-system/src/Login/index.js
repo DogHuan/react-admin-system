fbe82154667bc7d91d14e23833811935d17bc830
chengdongxu1997 <1759205313@qq.com>
2021-10-14 16:23:40 +0800

    添加背景图片

--- admin-system/src/Login/index.js
+++ admin-system/src/Login/index.js
@@ -1,12 +1,13 @@
 import React,{Component} from "react";
+import './index.css'
 export default class Login extends Component{
     constructor(props){
         super(props)
     }
     render(){
         return (
-            <div>
-                登录页面
+            <div className="login">
+                <p>登录页面</p>
             </div>
         )
     }
