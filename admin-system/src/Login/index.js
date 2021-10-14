c46914638dc92b28900199df1e84b17308d06cf8
chengdongxu1997 <1759205313@qq.com>
2021-10-14 17:01:27 +0800

    代码页面的调整和表单的设计

--- admin-system/src/Login/index.js
+++ admin-system/src/Login/index.js
@@ -1,33 +1,31 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { Form, Input, Button, Select } from 'antd';
-const { Option } = Select;
 import './index.css'
-export default class Login extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (
-        <div className="login">
-            <div className="loginForm">
-    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-      <Form.Item
-        name="note"
-        label="Note"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-    </Form>
-  );
-
-                <p>登录页面</p>
-                </div> 
-            </div>
-        )
-    }
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="login">
+        <div className="loginForm">
+         <p>登录页面</p>
+          <Form name="control-hooks" >
+            <Form.Item
+              name="user"
+              label="用户名"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Form>
+          )
+        </div>
+      </div>
+    )
+  }
 }
