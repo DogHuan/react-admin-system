02bda2ae722df507c1b4f40812f3349aae729982
chengdongxu1997 <1759205313@qq.com>
2021-10-19 14:01:26 +0800

    登录页面表单的添加和样式

--- admin-system/src/Login/index.js
+++ admin-system/src/Login/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createElement } from "react";
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './index.css'
@@ -6,61 +6,57 @@ export default class Login extends Component {
   constructor(props) {
     super(props)
   }
+  //此处使用的是表单的非受控组件，React.createRef()
+  formRef = React.createRef();
+  handleSumit = () => {
+    const formData = this.formRef.current.getFieldsValue([
+      'username', 'password',
+    ])
+    console.log("value", formData)
+  }
   render() {
     return (
       <div className="login">
         <div className="loginForm">
-        <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+          <Form className="login-form" ref={this.formRef}>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入你的用户名或账号！',
+                },
+              ]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="请输入用户名或账号" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入你的密码！',
+                },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="请输入密码"
+              />
+            </Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+            <Form.Item style={{
+              paddingTop: 20
+            }}>
+              <Button type="primary" htmlType="submit" onClick={this.handleSumit} >
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     )
