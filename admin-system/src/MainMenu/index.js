729534b47bde2e36abae58acdee50c9331240a8b
chengdongxu1997 <1759205313@qq.com>
2021-11-02 14:58:58 +0800

    菜单路由的保守版修改以及注释

--- admin-system/src/MainMenu/index.js
+++ admin-system/src/MainMenu/index.js
@@ -15,7 +15,7 @@ import Drags from '../Consumer/DragManage/Draggable'
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 //一、对函数组件的初步使用以及数据的存储
-//1、思考：这里我们一级目录和二级目录怎么去存储？怎么去取值？怎么去渲染不同级别目录的页面以及怎么存储的问题。
+//1、思考：如何选择存储一级目录和二级目录？怎么去取值？怎么去渲染不同级别目录的页面以及怎么存储的问题。
 //1.1、我们对系统要具备的功能进行配置，即需要什么角色，每个角色又拥有多少功能。也就转变为父菜单和子菜单。
 // 这里我们借鉴使用对象去存储数据，每个对象以角色去命名，设置key，title等属性，代表一级目录（父菜单）。
 // 同时每个对象内部嵌套一个childern孩子数组来存储二级目录（子菜单），设置key，title等属性，component来作为
@@ -94,18 +94,17 @@ const MainMenu = (props) => {
   //注意：Menu为左侧整个菜单栏，而菜单栏里的每一个级目录都统称菜单项
   //SubMenu是每个一级菜单项（父级菜单项），而Menu.Item是二级菜单（即子菜单项）。
 
-  //1、defaultOpenKeys为初始展开的SubMenu菜单项key数组,这里使用location对象属性pathname
-  //split分割出ip地址中SubMenu菜单项key数组赋值给defaultOpenKeys，保持菜单展开。
+  //1、defaultOpenKeys默认菜单项,获取当前地址栏window.location.pathname，去分割得到当前点击菜单项
+  //的动态类组件的组件名。
   const defaultOpenKeys = [window.location.pathname.split("/")?.[2]]
 
-  //2、openKeys当前展开的SubMenu菜单项key数组，把defaultOpenKeys赋值给openkey,菜单的展开随之改变。
-  //要使函数组件具有状态管理，可以useState() Hook,这样展开菜单就可以动态取值了。
+  //2、openKeys控制当前SubMenu父级菜单项是否展开，取defaultOpenKeys分割后的字符,保持父级菜单的动态展开。
+  //要使函数组件具有状态管理，可以使用钩子函数useState() Hook。
   const [openKeys, setOpenKeys] = useState(defaultOpenKeys)
 
-  //3、然后我们开始给菜单Menu添加回调函数，监听事件的变化。即这里对打开和关闭,为openKeys设置setOpenKeys。
+  //3、然后我们开始给菜单Menu添加回调函数，监听事件的变化。改变openKeys的状态。
   const handleChange =(keys)=>{
     const lastOpenKeys = keys.find(key =>openKeys.indexOf(key)===-1)
-    console.log("key",keys);
     for(var i=0; i<menuList.length; i++){
       if(menuList[i].key===lastOpenKeys){
         return setOpenKeys = (lastOpenKeys ? [lastOpenKeys] : [])
@@ -117,18 +116,31 @@ const MainMenu = (props) => {
   //设置role接收全局变量role角色，以及menuList存储菜单列表项
   const role = window.role
   let menuList = []
-  //4、我们已经取到了角色，开始为不同角色设置判断条件去判断其拥有的功能即菜单项。
-  //4.1、假设以数字1 or (admin)代表管理员, 数字2 or (normalUser)代表普通用户
-  //角色判断之后更新menuList数组存储菜单列表
+
+  //4、取到角色数组后，通过数组0或1判断role满足哪个角色。
+
+  //4.1、假设以数字0为false，1为true
+  //角色判断之后更新menuList数组存储菜单列表。数组role[0]代表管理员，role[1]代表审查员， 
+  //role[2]代表普通用户，紧接着依次添加角色。当角色为true更新menuList，push菜单对象。
   role?.[0] ==='1' && menuList.push(menuAdmin,menuUser)
-  role?.[1] === '2' && menuList.push(menuUser)
-  //4.2、设置defaultSelectedKeys初始选中的菜单项key数组，这里取ip路径
+  role?.[1] ==='1' && menuList.push(menuUser)
+
+  //4.2、defaultSelectedKeys初始被选中的菜单项，设置为路由link的值。
   const defaultSelectedKeys = [window.location.pathname]
-  //4.3、设置defaultRoute初始选中的菜单项路由，这里取menuList列表第一个孩子节点的第一个link值
+  //4.3、defaultRoute初始选中菜单的路由，这里取menuList列表第一个孩子节点的第一个link值
   const defaultRoute = menuList?.[0]?.children?.[0]?.link
 
+  //测试数据
+  let CmenuList = []
+  const CdefaultSelectedKeys = ''
+  const CopenKeys = CmenuList?.[0]?.children[0]
+  CmenuList.push(menuAdmin,menuUser)
+  const CdefaultRoute = CmenuList?.[0]?.children?.[0]?.link
+
   return (
-    //三、整体使用layout布局大框架包裹网页的整个页面
+    //三、整体使用layout布局大框架包裹其他两个layout,
+    //网页的整个页面三个部分Header(头部导航栏),Sider(左侧菜单栏),Content(中间内容),Footer(底部内容)
+
     <Layout style={{ minHeight: '100vh' }}>
       {/* 1、头部导航栏的设置和logo图标的添加，后期可以在这里维护退出登录和个人信息修改 */}
       <Header className="header">
@@ -144,28 +156,26 @@ const MainMenu = (props) => {
       添加Sider侧边栏组件，然后添加Meun菜单组件。*/}
       <Layout className="layout-main">
         <Sider className="layout-sider">
-        {/*2.1、 Menu中设置defaultSelectedKeys初始化选中的菜单项key数组以及
-        openKeys当前展开的SubMenu菜单项key数组，为SubMenu菜单父列表添加key，title */}
+        {/*2.1、 Menu中设置默认选择的菜单项属性defaultSelectedKeys以及
+        设置菜单项展开属性openKeys，添加监听事件。*/}
           <Menu
             theme="dark"
             mode="inline"
-            defaultSelectedKeys={defaultSelectedKeys}
-            openKeys={openKeys} 
+            defaultSelectedKeys={CdefaultSelectedKeys}
+            openKeys={CopenKeys} 
             onChange={handleChange}>
             {
-              // 2.2、此刻遍历menuList里item对象属性,即管理员或用户的数据值，
-              //一级目录SubMenu的title和key
-              menuList?.map(item => console.log("item", item)),
-              menuList?.map((item) => (
+              // 2.2、遍历menuList里item对象属性,即管理员或用户的数据值，
+              //一级目录（父菜单）SubMenu的title和key
+              CmenuList?.map((item) => (
                 <SubMenu
                   key={item.key}
                   icon={item.icon}
                   title={item.title}
                 >
-                  {/*2.3、 遍历父级菜单选项中的childern数组，为Menu.item菜单子列表中设置key，
-                  title以及路由Link的出口*/}
+                  {/*2.3、 遍历父级菜单选项中的childern数组，为Menu.item二级目录（子菜单）列表中
+                  设置key，title以及路由Link的出口*/}
                   {
-                    item?.children?.map(e => console.log("e.title", e.title)),
                     item?.children?.map((e) => (
                       <Menu.Item key={e.link} icon={e.icon}>
                         <Link to={e.link}>{e.title}</Link>
@@ -183,7 +193,7 @@ const MainMenu = (props) => {
             <Switch>
             {/* 3.1、Content渲染不同的菜单选项数据需要根据Route不同的路由入口来配置path，component路径 */}
               {
-                menuList?.map(item =>
+                CmenuList?.map(item =>
                   item?.children?.map(subItem =>
                     <Route
                       path={subItem.link}
@@ -191,8 +201,8 @@ const MainMenu = (props) => {
                     />
                   ))
               }
-              {/* 3.2、进入系统内部默认路由出口为mainAdmin */}
-              <Redirect to={defaultRoute || '/mainAdmin'} />
+              {/* 3.2、进入系统内部默认路由出口为mainMenu */}
+              <Redirect to={CdefaultRoute || '/mainMenu'} />
             </Switch>
           </Content>
           <Footer className="layout-footer" style={{margin:"auto"}}>Copyright © 2021</Footer>
