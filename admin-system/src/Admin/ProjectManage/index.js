205a5fbcfaad1c995c0ff01ba34179c5eee66c84
chengdongxu1997 <1759205313@qq.com>
2021-11-05 13:34:07 +0800

    云服务，ldap，smtp，项目，权限文件夹的增加

---
+++ admin-system/src/Admin/ProjectManage/index.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+export default class extends Component{
+    render(){
+        return(
+            <div>
+                项目管理
+            </div>
+        )
+    }
+}
