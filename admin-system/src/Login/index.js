30f979babffe5420f358d19b4af2c518277c1ce6
chengdongxu1997 <1759205313@qq.com>
2021-12-04 11:51:24 +0800

    清除login注释

--- admin-system/src/Login/index.js
+++ admin-system/src/Login/index.js
@@ -29,13 +29,7 @@ export default class Login extends Component {
       })
     }).then((response) => response.json()
     ).then((result) => {
-      if (result.code >= 200 || result.code < 300) {
-        console.log("登录成功")
-        // 二、思考：API连接成功后，要保持页面刷新后仍保留原本信息，而不是返回登录页面。
-        //1、保留原先登录信息即cookie本地存储的使用。js本地存储有cookie和store。
-        // 1.1、加载名为cookieName的cookie信息,cookie.load(cookieName)
-        cookie.load()
-        //1.2、判断是否勾选了记住密码选项，如果是则cooike保存密码，用户名，角色,token等信息
+      if (result.code ===200) {
         if (this.state.remember === true) {
           cookie.save({
             username: this.state.username,
@@ -44,7 +38,6 @@ export default class Login extends Component {
             token: result?.data?.token
           })
         }
-        //如果未勾选，则清除密码，保存用户名和其他信息
         else {
           cookie.remove(['password'])
           cookie.save({
@@ -53,11 +46,8 @@ export default class Login extends Component {
             token: result?.data?.token
           })
         }
-        // 当一个路由点击多次，将会出现警告，使用replace，这样会替换历史记录中上一次相同路由记录，
-        // 而push会添加新的相同记录进去，hash路由会有这个问题，Browser形式的则不会
         this.props.history.replace("/mainMenu")
       }
-      //4.4、对错误异常的处理
       else {
         message.error("登录失败" + result.msg)
       }
@@ -71,7 +61,6 @@ export default class Login extends Component {
       remeber:event.target.checked
     })
   }
-  //为密码加密
 
   handleChange=(e)=>{
     cookie.load(['username','password','role','token'])
@@ -121,19 +110,9 @@ export default class Login extends Component {
 
   render() {
 
-    //form表单校验方法1、使用validatemessages属性统一校验，先定义类型，再设置校验属性，属性值以及提示信息
-    //具体属性和属性值参考antd的validateMessages属性对应的GitHub上的代码
-    // const validateMessages = {
-    //   string: {
-    //     range: "${name} 必须在 ${min} 到 ${max} 之间",
-    //   },
-    // }
-
     return (
       <div className="login">
         <div className="loginForm">
-          {/* 给Form绑定initialValues属性，初始化表单的属性值。
-          然后绑定onFinish提交表单且数据验证成功后回调事件*/}
           <Form className="login-form" 
           initialValues={{
             remember:true,
@@ -143,7 +122,6 @@ export default class Login extends Component {
           onFinish={this.onFinish}>
             <Form.Item
               name="username"
-              //约束规则，rules
               rules={[
                 {
                   required: true,
@@ -161,20 +139,6 @@ export default class Login extends Component {
                   required: true,
                   message: '请输入你的密码！',
                 },
-                // {
-                //   type: 'string',
-                //   min: 2,
-                //   max: 10,
-                // }
-                //form表单校验方法2、逐个添加校验属性和属性值
-                // {
-                //   min: 4,
-                //   message: '密码必须不小于4位！',
-                // },
-                // {
-                //   max: 12,
-                //   message: '密码必须不大于12位！',
-                // },
               ]}
             >
               <Input.Password
